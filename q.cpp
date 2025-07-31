0e11eea52e274975c88815ba926df03296efcac6
carbonaria <151073884+carbonaria@users.noreply.github.com>
2025-07-31 12:02:37 +0300

    5

--- q.cpp
+++ q.cpp
@@ -2,12 +2,22 @@
 #include <cctype>
 #include <string>
 using namespace std;
-// взять, перевести нижний регистр в верхний и наоброт
-auto reg_trans(string q) {
+
+void write_help() {
+    cout << "usage: q.exe [options] [text]" << endl;
+    cout << "options:" << endl;
+    cout << "  --help\t display this message" << endl;
+}
+
+void write_version() {
+    cout << "Course work for option 4.3, created by Molchanova Margarita.\n";
+}
+// перевести нижний регистр в верхний и наоброт
+auto reg_trans(const string q) {
     string w;
-    for( int i=0; i<q.size(); i++) {
-        if (isupper(q[i])) w += tolower(q[i]);
-        else if (islower(q[i])) w += toupper(q[i]);
+    for( char s : q) {
+        if (isupper(s)) w += tolower(s);
+        else if (islower(s)) w += toupper(s);
         else{
             cout<<"err"<<endl;
             break;
@@ -15,15 +25,35 @@ auto reg_trans(string q) {
     }
     return w;
 }
+struct Config {
+    string text;
+    bool isInterupted;
+
+};
+
+//const?               *
+Config parse_arguments(int argc, const char* argv[]) {
+    Config cfg; //{ string, bool }
+    for (int i = 1; i < argc; i++) {
+        string arg = argv[i];
+        if (arg.rfind("--", 0) == 0) {
+            if (arg == "--help") write_help();
+            else if (arg == "--version") write_version();
+            else cout << "wrong argument: " << arg << endl;
+            cfg.isInterupted = true;
+        } else {
+            cfg.text += reg_trans(arg);
+        }
+    }
+    return cfg;
+}
 
 
-int main() {
-     
-    char q[100];
-    string w;
-    cout<<"dlsc:"<<endl;
-    cin>>q;
-    w=reg_trans(q);
-    cout<<w;
+
+
+int main(int argc, const char* argv[]) {
+    
+    Config pars_res = parse_arguments(argc, argv);
+    cout << pars_res.text;
     
 }
